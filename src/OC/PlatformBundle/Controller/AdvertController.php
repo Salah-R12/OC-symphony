dc9797df9a66580cf6c3ee3c8e9d8d8571f33fd9
Salah <radi.salah12@gmail.com>
2018-10-20 16:30:11 +0200

    routing / rajout de template /

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -12,8 +12,32 @@ class AdvertController extends Controller
 {
     public function indexAction()
     {
+
+        $url = $this->get('router')->generate('oc_platform_home', array(),
+            UrlGeneratorInterface::ABSOLUTE_URL);
         $content = $this->get('templating')->render('OCPlatformBundle:Advert:index.html.twig', array('nom' => 'winzou'));
 
+
+        return new Response($content);
+    }
+
+    public function viewAction($id)
+    {
+        // $id vaut 5 si l'on a appelé l'URL /platform/advert/5
+
+        // Ici, on récupèrera depuis la base de données
+        // l'annonce correspondant à l'id $id.
+        // Puis on passera l'annonce à la vue pour
+        // qu'elle puisse l'afficher
+        $content = $this->get('templating')->render('OCPlatformBundle:Advert:index.html.twig',
+                array('nom' => 'winzou','id' => $id));
+        return new Response($content);
+    }
+
+    public function viewSlugAction($slug, $year, $format)
+    {
+        $content = $this->get('templating')->render('OCPlatformBundle:Advert:index.html.twig',
+            array('nom' => 'winzou','id' => $format,'slug' => $slug,'format'=>$format));
         return new Response($content);
     }
 }
