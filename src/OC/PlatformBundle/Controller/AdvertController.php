168f154f0b631236b5d9423d55548ee30f014e37
Salah <radi.salah12@gmail.com>
2018-11-04 14:05:43 +0100

    Relation Many-To-One entre entity application et advert

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -12,6 +12,7 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use OC\PlatformBundle\Entity\Advert;
 use OC\PlatformBundle\Entity\Image;
+use OC\PlatformBundle\Entity\Application;
 
 class AdvertController extends Controller
 {
@@ -121,6 +122,45 @@ class AdvertController extends Controller
 
         // Étape 2 : On déclenche l'enregistrement
         $em->flush();
+
+        return $this->render('OCPlatformBundle:Advert:add.html.twig');
+    }
+
+    public function entityAction(Request $request)
+    {
+        $advert = new Advert();
+        $advert->setTitle('Recherche développeur Symfony.');
+        $advert->setAuthor('Alexandre');
+        $advert->setContent("Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…");
+        $advert->setDate(new \Datetime());
+        // Création d'une première candidature
+        $application1 = new Application();
+        $application1->setAuthor('Marine');
+        $application1->setContent("J'ai toutes les qualités requises.");
+        $application1->setDate( new \Datetime());
+        // Création d'une deuxième candidature par exemple
+        $application2 = new Application();
+        $application2->setAuthor('Pierre');
+        $application2->setContent("Je suis très motivé.");
+        $application2->setDate( new \Datetime());
+        // On lie les candidatures à l'annonce
+        $application1->setAdvert($advert);
+        $application2->setAdvert($advert);
+
+        // On récupère l'EntityManager
+        $em = $this->getDoctrine()->getManager();
+
+        // Étape 1 : On « persiste » l'entité
+        $em->persist($advert);
+
+        // Étape 1 ter : pour cette relation pas de cascade lorsqu'on persiste Advert, car la relation est
+        // définie dans l'entité Application et non Advert. On doit donc tout persister à la main ici.
+        $em->persist($application1);
+        $em->persist($application2);
+
+        // Étape 2 : On « flush » tout ce qui a été persisté avant
+        $em->flush();
+
 
         return $this->render('OCPlatformBundle:Advert:add.html.twig');
     }
